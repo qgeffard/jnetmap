de6bf4c02f274dd6a0651423e7ffaf9a0eee4afc
Florian <Florian@Florian-PC>
2012-12-27 22:26:57 +0100

    Affichage des prises n'étant reliée à aucune salle mais ayant un bandeau suivant le batiment.

--- jnetmap/src/main/java/org/sio/jnetmap/domain/Outlet.java
+++ jnetmap/src/main/java/org/sio/jnetmap/domain/Outlet.java
@@ -25,40 +25,56 @@ public class Outlet {
 
 	@ManyToOne
 	private Band bandOutlet;
-	
+
 	@SuppressWarnings("unchecked")
 	public static List<Outlet> findAllOutletsOrder() {
-        List<Outlet> l = entityManager().createNativeQuery("SELECT o.*, r.name_room FROM Outlet o, Room r, Band b WHERE o.id!=0 and o.room_outlet=r.id and o.band_outlet=b.id and o.room_Outlet != 0 ORDER BY r.name_room, o.num_outlet", Outlet.class).getResultList();
-        l.addAll(entityManager().createNativeQuery("SELECT o.*, d.name_dispatcher FROM Outlet o, Room r, Band b, Dispatcher d WHERE o.id!=0 and o.room_outlet=r.id and o.band_outlet=b.id and b.dispatcher_band=d.id and o.room_Outlet=0 ORDER BY d.name_dispatcher, b.num_band, o.num_outlet", Outlet.class).getResultList());
+		List<Outlet> l = entityManager()
+				.createNativeQuery(
+						"SELECT o.*, r.name_room FROM Outlet o, Room r, Band b WHERE o.id!=0 and o.room_outlet=r.id and o.band_outlet=b.id and o.room_Outlet != 0 ORDER BY r.name_room, o.num_outlet",
+						Outlet.class).getResultList();
+		l.addAll(entityManager()
+				.createNativeQuery(
+						"SELECT o.*, d.name_dispatcher FROM Outlet o, Room r, Band b, Dispatcher d WHERE o.id!=0 and o.room_outlet=r.id and o.band_outlet=b.id and b.dispatcher_band=d.id and o.room_Outlet=0 ORDER BY d.name_dispatcher, b.num_band, o.num_outlet",
+						Outlet.class).getResultList());
 		return l;
-    }
-	
+	}
+
 	@SuppressWarnings("unchecked")
 	public static List<Outlet> findOutletsOfBuilding(Long id) {
-		return entityManager().createNativeQuery(
-				"SELECT o.* FROM Outlet o, Room r, Building b WHERE o.room_outlet=r.id and r.building_room=b.id and b.id="+id+" ORDER BY r.name_room, o.num_outlet", Outlet.class)
-				.getResultList();
+		List<Outlet> l = entityManager()
+				.createNativeQuery(
+						"SELECT o.* FROM Outlet o, Room r, Building b WHERE o.room_outlet=r.id and r.building_room=b.id and b.id="
+								+ id + " ORDER BY r.name_room, o.num_outlet",
+						Outlet.class).getResultList();
+		l.addAll(entityManager()
+				.createNativeQuery(
+						"SELECT o.*, d.name_dispatcher FROM Outlet o, Room r, Band ba, Dispatcher d, Building b WHERE b.id=d.building_dispatcher and o.id!=0 and o.room_outlet=r.id and o.band_outlet=ba.id and ba.dispatcher_band=d.id and o.room_Outlet=0 and b.id="+id+"ORDER BY d.name_dispatcher, ba.num_band, o.num_outlet",
+						Outlet.class).getResultList());
+		return l;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static List<Outlet> findOutletsOfRoomAndBand(Long idRoom, Long idBand) {
 		return entityManager().createNativeQuery(
-				"SELECT o.* FROM Outlet o, Room r WHERE o.room_outlet=r.id and  o.room_outlet=" + idRoom
-						+ " and o.band_outlet=" + idBand+" ORDER BY r.name_room, o.num_outlet", Outlet.class)
+				"SELECT o.* FROM Outlet o, Room r WHERE o.room_outlet=r.id and  o.room_outlet="
+						+ idRoom + " and o.band_outlet=" + idBand
+						+ " ORDER BY r.name_room, o.num_outlet", Outlet.class)
 				.getResultList();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static List<Outlet> findOutletsOfRoom(Long idRoom) {
 		return entityManager().createNativeQuery(
-				"SELECT o.* FROM Outlet o, Room r WHERE o.room_outlet=r.id and o.room_outlet=" + idRoom+" ORDER BY r.name_room, o.num_outlet", Outlet.class)
-				.getResultList();
+				"SELECT o.* FROM Outlet o, Room r WHERE o.room_outlet=r.id and o.room_outlet="
+						+ idRoom + " ORDER BY r.name_room, o.num_outlet",
+				Outlet.class).getResultList();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static List<Outlet> findOutletsOfBand(Long idBand) {
 		return entityManager().createNativeQuery(
-				"SELECT o.* FROM Outlet o, Room r WHERE o.room_outlet=r.id and o.band_outlet=" + idBand+" ORDER BY r.name_room, o.num_outlet", Outlet.class)
-				.getResultList();
+				"SELECT o.* FROM Outlet o, Room r WHERE o.room_outlet=r.id and o.band_outlet="
+						+ idBand + " ORDER BY r.name_room, o.num_outlet",
+				Outlet.class).getResultList();
 	}
 }
